9f6453d62df0ec14d41ca06017f3008115a9a522
endless-lotus <astronomy.vanessa@gmail.com>
2024-08-14 12:51:40 -0400

    Balance/Currency Update - Casino Class is removed - Global Variables other than game rules are removed + function for random number in a list for length of roulette numbers + calculations for bet based on random number(ball being spun on roulette) + currency update with win or loss + True or False statement for win or loss for ease of tracking

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import random
 
 #Game Rules Can be here
 #table view so it takes up set amount of room!
-wheel = list(range(1, 37))
-spin_number = random.choice(wheel)
 red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 #bets
@@ -28,53 +26,90 @@ def table_view():
                                 |     First Dozen    |        Second Dozen       |      Third Dozen      |                                                                   
                  """)
 
-#Player: Ask for BET, Ask for Starting Balance
+
+#Player: have a print statement return info: money lost, money won, current balance
 class Player: 
-    def __init__(self, name, balance, bet, lost_money, won_money):
+    def __init__(self, name, balance, bet_type, bet, bet_value, is_watching_ball_roll, lost_money, won_money):
         self.name = name # = player's name input
         self.balance = balance # = player's current monetary value
-        self.bet = '' # = player's bet on current game
-        self.lost_money = 0 # holds value of money lost and maths it to balance
-        self.won_money = 0 #holds value of won money and maths it to balance
+        self.bet_type = ''
+        self.bet = '' # = player's current variable value of bet
+        self.bet_value = ''
+        self.is_watching_ball_roll = ''
+        self.lost_money = 0 #True or False to money being lost
+        self.won_money = False #True or False to money being won
+
 
     def is_picking_bet(self):
-        player_bet_num = int(input("""
+        self.bet_type = int(input("""
 ♠♤♠**♡♥♡**♦♢♦**♧♣♧**♠♤♠**♡♥♡**♦♢♦**♧♣♧**♠♤♠**♡♥♡**♦♢♦**♧♣♧**♠♤♠**♡♥♡**♦♢♦**♧♣♧**♠♤♠
 Please pick a number for the available bets:
  [1] {bet_a}
               """.format(bet_a=a)))
-        if player_bet_num == 1:
-             player_bet_num = int(input("""
+        if self.bet_type == 1:
+             player_bet = int(input("""
 Please pick which color you would like to place your bet on:
  [1] {bet_a1}
  [2] {bet_a2}
                                     """.format(bet_a1=a_1, bet_a2=a_2)))
-             if player_bet_num == 1:
+             if player_bet == 1:
                   self.bet = a_1
              else:
                   self.bet = a_2     
         else:
              print("Invalid Input please try again.")
-        return self.bet
+        return self.bet, print(self.bet_type)
         
     def is_placing_bet(self):
-         player_bet_value = int(input("For your bet placed on {bet} how much money would you like to pin on that partner? Your current balance is: {balance}\n".format(bet=self.bet, balance=self.balance)))
-         if player_bet_value > self.balance:
+         self.bet_value = int(input("For your bet placed on {bet} how much money would you like to pin on that partner? Your current balance is: {balance}\n".format(bet=self.bet, balance=self.balance)))
+         if self.bet_value > self.balance:
               print("Woah there Partner! You don't have that much in your balance, how about you try that again.")
-         elif player_bet_value <= 0:
+         elif self.bet_value <= 0:
              print("Inadequte amount for bet.")
          else:
-            print("The ball is now rolling as all bets have been placed. . .")
-         return player_bet_value
-        
-    #player_lost_money
+            table_view()
+            print("The ball is now rolling as all bets have been placed. . .") 
+         return self.bet_value #This Works!!!! returns int input for bet!
     
-    #player_won_money
+    def is_watching_the_ball_roll(self):
+        wheel = list(range(1, 37))
+        self.is_watching_ball_roll = random.choice(wheel)
+        return print("The ball has landed on {number}!".format(number=self.is_watching_ball_roll)) #this works!!! I'm on a roll ^o^
+        
+    #FUCK YES IT WORKS --> was using wrong iteration of loops 
+    def is_calculating_bet(self):
+        if self.bet == a_1:
+            if self.is_watching_ball_roll in red:
+                self.won_money = True
+            else:
+                self.won_money = False
+        elif self.bet == a_2:
+            if self.is_watching_ball_roll in black:
+                self.won_money = True
+            else:
+                self.won_money = False
+        else:
+            print("ERROR")
+        return self.won_money   
 
-
-#Casino: show table, roll wheel, bet ends set up another bet, types of bets? Needs 6
-#
-
+    def is_calculating_balance(self):
+        if self.won_money == True:
+            #For different types of bets on roulette
+            if self.bet_type == 1: #checking to see if its equal to a bet on Red or Black
+                #For a 1 to 1 ratio
+                self.balance += self.bet_value
+            else:
+                print("ERROR")
+        elif self.won_money == False:
+            #For self.won_money == False
+            self.balance -= self.bet_value
+        else:
+            print("ERROR")
+        return print(self.balance) #this works and is able to be used for later implementations
+    
+#print("🔴\nCongrats {player} you have won ${bet} on your bet for {bet_type}!".format(player=self.name, bet=self.bet_value, bet_type=self.bet))
+#print("🔴⚫\n{player} its seems you have lost ${bet} on your bet for {bet_type}".format(player=self.name, bet=self.bet_value, bet_type=self.bet))
+#print("Your current balance is: ${balance}".format(balance=self.balance))
 
 #Get inf for Player:
 player_name = input("""
@@ -89,9 +124,11 @@ How much rootin' tootin' money would you like to play with?
                  """.format(name=player_name)))
 
 #To Run Code Between Player and Casino
-player = Player(player_name, player_balance, 0, 0, '')
-#casino = Casino(player_balance)
+player = Player(player_name, player_balance,'', '', '', '', 0, False)
 
 #casino.display_game_rules()
 player.is_picking_bet()
 player.is_placing_bet()
+player.is_watching_the_ball_roll()
+player.is_calculating_bet()
+player.is_calculating_balance()
